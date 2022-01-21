6d2e90c88384d0a02d3b6a853bb7ef66c5907eea
Manan Gouhari <gouhari21@hotmail.com>
2022-01-22 02:06:16 +0530

    code to make an oven on Kolibri

--- index.js
+++ index.js
@@ -3,12 +3,30 @@ const { InMemorySigner } = require("@taquito/signer");
 
 require("dotenv").config();
 
+const contracts = {
+  OvenFactory: "KT1Mgy95DVzqVBNYhsW93cyHuB57Q94UFhrh",
+};
+
 const Tezos = new TezosToolkit("https://mainnet.smartpy.io/");
 
 async function main() {
   Tezos.setProvider({
     signer: await InMemorySigner.fromSecretKey(process.env.PRIVATE_KEY),
   });
+
+  await makeOven();
 }
 
 main();
+
+async function makeOven(Tezos) {
+  const ovenFactory = await Tezos.wallet.at(contracts.OvenFactory);
+  try {
+    const op = await ovenFactory.methods.makeOven().send();
+    console.log("Operation Sent -- Confirming block.");
+    await op.confirmation(3);
+    console.log(`Operation Confirmed: ${op.opHash}`);
+  } catch (err) {
+    console.log(err);
+  }
+}
