3090d9066a4ac87de75157e44f032c3a10d2b4b3
Manan Gouhari <gouhari21@hotmail.com>
2022-01-22 14:44:54 +0530

    logic to estimate kUSD->TEZ swap amount

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
-const { TezosToolkit, OpKind } = require("@taquito/taquito");
+const { TezosToolkit, OpKind, MichelCodecPacker } = require("@taquito/taquito");
 const { InMemorySigner } = require("@taquito/signer");
+const { BigNumber } = require("bignumber.js");
 
 require("dotenv").config();
+
+const FEE_FACTOR = 997;
 
 const contracts = {
   OvenFactory: "KT1Mgy95DVzqVBNYhsW93cyHuB57Q94UFhrh",
   Oven: "KT19cVLk9EK95rV1cF24hv34aACAqC7birBA",
+  KolibriUSD: "KT1K9gCRgaLRFKTErYt1wVxA3Frb9FjasjTV",
+  QuipuKolibriDEX: "KT1K4EwTpbvYN9agJdjpyJm4ZZdhpUNKB3F6",
 };
 
 const Tezos = new TezosToolkit("https://mainnet.smartpy.io/");
 
 async function main() {
+  Tezos.setPackerProvider(new MichelCodecPacker());
   Tezos.setProvider({
     signer: await InMemorySigner.fromSecretKey(process.env.PRIVATE_KEY),
   });
 
-  await leverageTez(Tezos, Math.pow(10, 1), 5 * Math.pow(10, 17));
+  // await leverageTez(Tezos, Math.pow(10, 1), 5 * Math.pow(10, 17));
+  console.log(
+    (await estimatemKUSDtoTEZ(Tezos, BigNumber(10).pow(20))).toString()
+  );
 }
 
 main();
@@ -63,4 +72,17 @@ async function makeOven(Tezos) {
   } catch (err) {
     console.log(err);
   }
+}
+
+async function estimatemKUSDtoTEZ(Tezos, kUSDAmount) {
+  kolibriDEX = await Tezos.wallet.at("KT1K4EwTpbvYN9agJdjpyJm4ZZdhpUNKB3F6");
+  const dexStorage = await kolibriDEX.storage();
+  // console.log(dexStorage.storage.tez_pool.div(1e6).toString());
+  const tokenInWithFee = kUSDAmount.times(FEE_FACTOR);
+  const numerator = tokenInWithFee.times(dexStorage.storage.tez_pool);
+  const denominator = dexStorage.storage.token_pool
+    .times(1000)
+    .plus(tokenInWithFee);
+
+  return numerator.idiv(denominator);
 }
